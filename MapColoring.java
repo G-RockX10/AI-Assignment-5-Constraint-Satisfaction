12c0f67d3d7a75eee125302a80897912bcc62b92
G-RockX10 <geraldt.belton@gmail.com>
2022-11-18 13:54:53 -0500

    Update MapColoring.java

--- MapColoring.java
+++ MapColoring.java
@@ -16,10 +16,26 @@ public class MapColoring {
  // not done
     public void start(){
          int regionNum;
+         int connections;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Chose how many Regions");
         regionNum = sc.nextInt();
+        
+        
+        for(int i = 0 ; 0< regionNum; i++){
+            Region newRegion; // initalize 
+            
+            
+           region.add(i, newRegion);
+        }
+        
+        System.out.println("Chose how many Connections");
+        connections = sc.nextInt();
+        
+        
+        
+        
     }
     
     
@@ -32,18 +48,31 @@ public class MapColoring {
     // not done
     // find regions not yet assinged color
      public Region selectRegion() {
-         
+         // fix
+   for (int i = 0; i <   region.connections.size(); i++){    
         
-        
+      if(  region.get(i).getColor()!= null){
+       
          // if all regions have color return null
          return null;
+      }
+      else return region.get(i);
      }
-    
-    // not done
+   return null;
+     }
+    // test
      // check if any neabor region has color return true if so
     public boolean checkColor(Region test, String color) {
        
-        test.getColor()
+        
+       
+     for (int i = 0; i <   test.connections.size(); i++){
+      Region neighbor = region.get( test.connections.get(i));
+    if(  neighbor.getColor()== color ){
+        return true;
+    }
+        
+     }
      
         
                 
@@ -51,7 +80,7 @@ public class MapColoring {
         return false;
     }
 
-    //test
+    //not done
     public MapColoring copy() {
 
         MapColoring copyOfMe = new MapColoring();
@@ -72,8 +101,13 @@ public class MapColoring {
     
       // not done
     public void play() {
-       
+       start(); // test
 
+       
+       
+       
+       
+       
     }
     
     
